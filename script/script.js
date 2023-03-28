66c8a604259c803c73022926a1ad6507e1381155
Omkar <Omkar.Patil@efs-auto.com>
2023-03-28 14:02:21 +0200

    updated animation

--- script/script.js
+++ script/script.js
@@ -1,12 +1,12 @@
 let holder = document.getElementById('background')
-let gs = 42;
+let gs = 35;
 
 window.onload = () => {
     for (let i =0; i < gs; i++){
         for (let j =0; j < gs; j++){
             let dot = document.createElement("div")
             dot.classList.add("dot")
-            dot.style.animationDelay = '${i * j}ms'
+            dot.style.animationDelay = `${Math.sin(i * j)/7}s`
             holder.appendChild(dot)
         }
     }
